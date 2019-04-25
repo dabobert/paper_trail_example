e4b0fba42dd63d319e03a6183bba6466d7f9238f
Robert Jenkins <bobert@gmail.com>
2019-04-25 12:18:55 -0400

    implemented paper_trail

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  has_paper_trail
+
+  def self.make
+    Post.create! :name => Faker::Book.title, :author => Faker::Name.name, :body => Faker::Lorem.paragraphs(6).join(" ").strip
+  end
+
 end
